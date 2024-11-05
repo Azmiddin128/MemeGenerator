c29ef0bc22b5c80b9b2a150177b5f2512212149c
Azmiiddiin <azmiddin48@gmail.com>
2024-11-05 17:13:05 +0300

    feat: anabled to choose a picture

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ top_text = input("Enter top text: ")
 bottom_text = input("Enter bottom text: ")
 
 print(top_text, bottom_text)
+
+print('List of Images:')
+print("1. Cat in restaurant")
+print("2. Cat in glasses")
+print("3. Surprised cat")
+
+chose_picture = input("Enter the number of your choice: ")
+
+if chose_picture == "1":
+    print("You chose: cat in restaurant")
+    path_to_picture = "cat_in_restaurant.png"
+elif chose_picture == "2":
+    print("You chose: cat in glasses")
+    path_to_picture = "cat_in_glasses.png"
+elif chose_picture == "3":
+    print("You chose: surprised cat")
+    path_to_picture = "surprised_cat.png"
+
+print(path_to_picture)
